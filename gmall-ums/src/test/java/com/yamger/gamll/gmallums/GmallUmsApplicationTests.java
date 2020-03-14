9abe51c112341436a736946e5edb652ec758c5b1
@Chanhy1 <cedarsy@outlook.com>
2020-03-14 13:40:17 +0800

    项目结构创建

---
+++ gmall-ums/src/test/java/com/yamger/gamll/gmallums/GmallUmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamger.gamll.gmallums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
